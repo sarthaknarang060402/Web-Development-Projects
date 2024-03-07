bb732a45814d8772b89f09cbaccea70b0b0b5aa2
Sarthak Narang <91564487+sarthaknarang060402@users.noreply.github.com>
2024-03-07 19:38:00 +0000

    exprrss

--- Backend/3.4 Middleware/index4.js
+++ Backend/3.4 Middleware/index4.js
@@ -1,7 +1,30 @@
 import express from "express";
-
+// import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+var bandName = "";
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+function bandNameGenerator(req, res, next) {
+  console.log(req.body);
+  bandName = req.body["street"] + req.body["pet"];
+  next();
+}
+
+app.use(bandNameGenerator);
+
+app.post("/submit", (req, res) => {
+  console.log(req.body);
+  res.send(`<h1>Your band name is:</h1><h2>${bandName}✌️</h2>`);
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
